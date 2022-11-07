b94050cf2a26433a38205f89417bcf48d2df6442
ChristianFerrario <chri.fer.emi@gmail.com>
2022-11-07 21:52:50 +0100

    Instatiated Socket.io server  * on connect and on disconnect functions (barebone))

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});

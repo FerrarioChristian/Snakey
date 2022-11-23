bc1742d930c2b5a754560b93bb24e1e7e7d8f0c6
ChristianFerrario <chri.fer.emi@gmail.com>
2022-11-23 17:03:55 +0100

    First working socket communication from frontend  * fixed wrong commit that omitted delete of startingPosition.ts in favour of startingPosition.js

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,42 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:3000");
 
 function App() {
-  return <div>Snakey</div>;
+  const [isConnected, setIsConnected] = useState(false);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      setIsConnected(true);
+      console.log("Connected");
+    });
+
+    socket.on("disconnect", () => {
+      setIsConnected(false);
+    });
+
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+    };
+  }, []);
+
+  const playerMoved = () => {
+    socket.emit("playerMoved", "left", (data: any) => {
+      setMessage(data);
+      console.log(data);
+    });
+  };
+
+  return (
+    <div>
+      <p>Connected: {"" + isConnected}</p>
+      <p>Message: {"" + JSON.stringify(message)}</p>
+      <button onClick={playerMoved}>Send ping</button>
+    </div>
+  );
 }
 
 export default App;

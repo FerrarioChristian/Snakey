306fa3f826c5a9a1bc6731abe36aa578985e72fb
ChristianFerrario <chri.fer.emi@gmail.com>
2022-11-30 23:51:55 +0100

    Implemented winning message

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,28 @@ import { init, render } from "./gameEngine";
 import { socket } from "./socket";
 
 function App() {
+  const [winner, setWinner] = useState("");
+
   useEffect(() => {
     init();
     socket.on("connect", () => {
       console.log("Connected");
     });
 
-    socket.on("disconnect", () => {
-      console.log("Disconnected");
+    socket.on("gameStart", () => {});
+    socket.on("gameOver", (winner) => {
+      if (winner === 3) {
+        setWinner("Draw");
+      }
+      if (winner === 2) {
+        setWinner("Player 2");
+      }
+      if (winner === 1) {
+        setWinner("Player 1");
+      }
     });
 
-    socket.on("statusUpdate", (gameState) => {
+    socket.on("stateUpdate", (gameState) => {
       render(gameState);
     });
 
@@ -34,6 +45,7 @@ function App() {
     <>
       <div id="gameScreen">
         <div>
+          <p>winner: {winner}</p>
           <canvas id="canvas"></canvas>
         </div>
       </div>
--- frontend/src/gameEngine.js
+++ frontend/src/gameEngine.js
@@ -1,41 +1,41 @@
 import { socket } from "./socket.js";
-let canvas, ctx;
+let canvas, context;
 
 export const init = () => {
   canvas = document.getElementById("canvas");
-  ctx = canvas?.getContext("2d");
+  context = canvas.getContext("2d");
 
   canvas.width = canvas.height = 600;
 
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = "black";
+  context.fillRect(0, 0, canvas.width, canvas.height);
 
   document.addEventListener("keydown", (e) => {
-    socket.emit("keydown", e.key);
+    socket.emit("playerMoved", e.key);
   });
 
-  return { canvas, ctx };
+  return { canvas, context };
 };
 
 export const render = (gameState) => {
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = "black";
+  context.fillRect(0, 0, canvas.width, canvas.height);
 
   const food = gameState.food;
   const size = canvas.width / 20;
 
-  ctx.fillStyle = "pink";
-  ctx.fillRect(food.x * size, food.y * size, size, size);
+  context.fillStyle = "pink";
+  context.fillRect(food.x * size, food.y * size, size, size);
 
-  paintPlayer(gameState.player1, size, "blue");
-  paintPlayer(gameState.player2, size, "red");
+  paintSnake(gameState.player1, size, "blue");
+  paintSnake(gameState.player2, size, "red");
 };
 
-function paintPlayer(playerState, size, colour) {
+function paintSnake(playerState, size, colour) {
   const body = playerState.body;
 
-  ctx.fillStyle = colour;
+  context.fillStyle = colour;
   for (let cell of body) {
-    ctx.fillRect(cell.x * size, cell.y * size, size, size);
+    context.fillRect(cell.x * size, cell.y * size, size, size);
   }
 }
